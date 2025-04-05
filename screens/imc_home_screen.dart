d75b9c76d76dd29dc170f20cc92650b66c26d286
Jesus Miguel Rodriguez <52975938+ning3n@users.noreply.github.com>
2025-04-04 21:32:18 -0400

    Add files via upload

--- screens/imc_home_screen.dart
+++ screens/imc_home_screen.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:imc_calculator/components/gender_selector.dart';
+import 'package:imc_calculator/components/height_selector.dart';
+import 'package:imc_calculator/components/number_selector.dart';
+import 'package:imc_calculator/core/app_colors.dart';
+import 'package:imc_calculator/core/app_styles.dart';
 
 class ImcHomeScreen extends StatefulWidget {
   const ImcHomeScreen({super.key});
@@ -9,11 +13,58 @@ class ImcHomeScreen extends StatefulWidget {
 }
 
 class _ImcHomeScreenState extends State<ImcHomeScreen> {
+  int selectedAge = 20;
+  int selectedWeight = 80;
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        GenderSelector()
+        GenderSelector(),
+        HeightSelector(),
+        Padding(
+          padding: const EdgeInsets.all(16),
+          child: Row( children: [
+            Expanded(child: NumberSelector(title: "PESO", value: selectedWeight , 
+            onIncrement: () {  
+              setState(() {
+                selectedWeight++;
+              });
+            },
+            onDecrement:() {
+              setState(() {
+                selectedWeight--;
+              });
+            }
+            )),
+            SizedBox(width: 16),
+            Expanded(child: NumberSelector(title: "EDAD", value: selectedAge , 
+            onIncrement: () {  
+              setState(() {
+                selectedAge++;
+              });
+            },
+            onDecrement:() {
+              setState(() {
+                selectedAge--;
+              });
+            }
+            )),
+          ],),
+        ),
+        Spacer(),
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: SizedBox(
+            height: 60,
+            width: double.infinity,
+            child: ElevatedButton(onPressed: (){}, style: ButtonStyle(
+              shape: WidgetStateProperty.all(RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8)
+              )),
+              backgroundColor: WidgetStateProperty.all(AppColors.primary),
+            ) , child: Text("Calcular", style: TextStyles.bodyText,))
+            ),
+        )
       ],
     );
   }
